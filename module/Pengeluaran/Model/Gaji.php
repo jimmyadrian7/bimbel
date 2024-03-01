59cc657125f95c71fbd0494153d77772c57bd431
EGELAZE\jimmy <jimmyadrian77@yahoo.com>
2024-03-01 17:24:08 +0700

    Fix bug report

--- module/Pengeluaran/Model/Gaji.php
+++ module/Pengeluaran/Model/Gaji.php
@@ -173,12 +173,12 @@ class Gaji extends BaseModel
                     END
                 AS persen_komisi'), DB::raw('(tagihan_detail.total / IFNULL(tagihan_detail.bulan, 1)) AS total_terbagi')
             )
-            ->leftJoin('tagihan', 'tagihan.id', 'tagihan_detail.tagihan_id')
-            ->leftJoin('siswa', 'siswa.id', 'tagihan.siswa_id')
-            ->leftJoin('orang', 'orang.id', 'siswa.orang_id')
-            ->leftJoin('kursus', 'kursus.id', 'tagihan.kursus_id')
-            ->leftJoin('pembiayaan', 'pembiayaan.id', 'tagihan_detail.pembiayaan_id')
-            ->leftJoin('transaksi', function($join) {
+            ->join('tagihan', 'tagihan.id', 'tagihan_detail.tagihan_id')
+            ->join('siswa', 'siswa.id', 'tagihan.siswa_id')
+            ->join('orang', 'orang.id', 'siswa.orang_id')
+            ->join('kursus', 'kursus.id', 'tagihan.kursus_id')
+            ->join('pembiayaan', 'pembiayaan.id', 'tagihan_detail.pembiayaan_id')
+            ->join('transaksi', function($join) {
                 $join->on('transaksi.tagihan_id', 'tagihan.id')
                     ->on('transaksi.id', DB::raw('(SELECT id FROM transaksi WHERE tagihan_id = tagihan.id ORDER BY id DESC LIMIT 1)'));
             });
@@ -199,8 +199,11 @@ class Gaji extends BaseModel
 
         $query = $this->baseQuery();
         $query->addSelect(['komisi' => DB::raw('
-            CASE WHEN year(tagihan.tanggal_lunas) = year(' . $tanggal_gaji . ') AND month(tagihan.tanggal_lunas) = month(' . $tanggal_gaji . ')  THEN
+            CASE 
+            WHEN year(tagihan.tanggal_lunas) = year(' . $tanggal_gaji . ') AND month(tagihan.tanggal_lunas) = month(' . $tanggal_gaji . ')  THEN
                 (TIMESTAMPDIFF(MONTH, tagihan.tanggal_lunas, tagihan_detail.tanggal_iuran_mulai) + 1 ) * tagihan_detail.komisi
+            WHEN year(tagihan.tanggal_lunas) > year(' . $tanggal_gaji . ') OR month(tagihan.tanggal_lunas) > month(' . $tanggal_gaji . ')  THEN
+                0
             ELSE
                 tagihan_detail.komisi
             END AS komisi
