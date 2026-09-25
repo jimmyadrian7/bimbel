f746e1ee530198a97b7d420c2180e445dbaf0ce2
EGELAZE\jimmy <jimmy>
2026-09-25 21:01:44 +0700

    adding route to export preview tagihan

--- module/Report/_routes/fetch_tagihan.php
+++ module/Report/_routes/fetch_tagihan.php
@@ -19,3 +19,23 @@ $app->post("/api/generate/report/tagihan_list/excel", function ($request, $respo
     $response->getBody()->write($result);
     return $response;
 });
+
+$app->post("/api/generate/report/tagihan_preview", function ($request, $response, $args) {
+
+    $controller = $this->get("Bimbel\Report\Controller\TagihanExportController");
+    $result = $controller->exportPreviewPdf($request, $args, $response);
+
+    $response = $response->withHeader("Content-Type", "application/json");
+    $response->getBody()->write($result);
+    return $response;
+});
+
+$app->post("/api/generate/report/tagihan_preview/excel", function ($request, $response, $args) {
+
+    $controller = $this->get("Bimbel\Report\Controller\TagihanExportController");
+    $result = $controller->exportPreviewExcel($request, $args, $response);
+
+    $response = $response->withHeader("Content-Type", "application/json");
+    $response->getBody()->write($result);
+    return $response;
+});
